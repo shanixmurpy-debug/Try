1f35bba86045ad42567dcd9b9eae1989d23885b8
shanixmurpy-debug <shanixmurpy@gmail.com>
2026-03-14 23:08:51 +0800

    Add files via upload

---
+++ Marie's Sari Sari Store/index.js
@@ -0,0 +1,7 @@
+let username;
+
+document.getElementById("mySubmit").onclick = function() {
+    username = document.getElementById("myText").value;
+    document.getElementById("myH1").textContent = "Hello, " + username + "!";
+
+}
